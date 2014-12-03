0bedf46e494ea3ad903fcad4b92b564c63af777f
Eric Buckthal <ebuckthal@gmail.com>
2014-12-03 09:26:16 -0800

    more files?

---
+++ js/draw.js
@@ -0,0 +1,15 @@
+var w = Math.min(Math.max(document.documentElement.clientWidth, window.innerWidth || 0), 1000);
+var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
+
+function setup() {
+
+
+    var c = createCanvas(w, h);
+    c.parent('canvas-container');
+
+}
+
+
+function draw() {
+
+}
